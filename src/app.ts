7fa8f9fa435c1796a5a8de790906c79feddd2532
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-03-02 02:13:25 +0600

    aded errors and middlewares functionality

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -10,13 +13,13 @@ app.use(cookieParser());
 app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 
 // application routes
-// app.use('/api', router);
+app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Backend Software Running!');
 });
 
-// app.use(globalErrorHandler);
-// app.use(notFound);
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
